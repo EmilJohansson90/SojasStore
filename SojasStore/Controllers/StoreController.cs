e2b1ac0f9c27aad334c6d10920157c392e031627
Emil <emil.johansson@studerande-skys.se>
2022-01-18 11:09:22 +0100

    Fix

---
+++ SojasStore/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private StoreService _storeService;
+
+        public StoreController()
+        {
+            _storeService = new StoreService();
+        }
+
+
+    }
+}
